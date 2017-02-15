c8782a9f9b200e695d61f177e6637bb0d17b35c2
Jake LaBossier <jakelaboss@gmail.com>
2017-02-14 21:37:14 -0500

    Added a few more files to track

---
+++ lisp/network.lisp
@@ -0,0 +1,16 @@
+(in-package :stumpwm)
+
+(defun manual-network-test (SSID passphrase)
+    (format nil "wpa_supplicant -B -i wlp3s0 -c <(wpa_passphrase \"~a\" \"~a\")" SSID passphrase))
+
+
+
+;; process to create a ssh link between local 4006 port and port on remote 4005 port
+;; ssh -L4006:127.0.0.1:4005 192.168.0.100
+
+;; create ssh link between x11vnc on remote and on localhost:0
+;; ssh -t -L 5900:localhost:5900 192.168.0.100 'sudo x11vnc -display :0 -auth /home/arch/.Xauthority'
+
+;; to connect to localhost:0
+;; vncviewer localhost:0
+
