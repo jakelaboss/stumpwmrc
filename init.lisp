3491f81e89a721c917591e16232f1c2b14e1a528
Jake LaBossier <jakelaboss@gmail.com>
2019-09-23 10:50:46 -0400

    Init file

--- init.lisp
+++ init.lisp
@@ -1,7 +1,7 @@
 
 ;;; init.lisp --- Vital settings and loading other files
 
-;; Copyright © 2013-2016 Jake LaBossier <jakelaboss@gmail.com>
+;; Copyright © 2013-2019 Jake LaBossier <jakelaboss@gmail.com>
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -9,19 +9,12 @@
 ;;; Commentary:
 
 ;; This file should be symlinked to "~/.stumpwmrc".
-;; I compile stumpwm with swank, so i don't need to load it. (but I do anyway to be safe)
+;; I compile stumpwm with swank, so i don't need to load it.
 
 ;; Code:
 ;;------------------------------------------------------------------------------------------------------------------------ ;;
 
 (in-package :stumpwm)
-
-;; (load "/home/vagabond/.emacs.d/elpa/slime-20170209.1240/swank-loader.lisp")
-
-;; (swank-loader:init)
-
-;; (load "/home/vagabond/common-lisp/libraries/linux/stumpwm/lisp/startup.lisp")
-;; (load "/home/vagabond/common-lisp/libraries/linux/stumpwm/lisp/commands/tile-group.lisp")
 
 (mapcar #'load '(
                  ;; "/home/vagabond/common-lisp/libraries/linux/stumpwm/lisp/load.lisp"
@@ -37,5 +30,3 @@
                   ))
 
 ;;; END init.lisp
-
-;; ssh -i "/path/to/keyfile" root@ec2-54-173-185-85.compute-1.amazonaws.com
